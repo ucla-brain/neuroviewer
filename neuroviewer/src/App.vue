5863bfc14eb13d5289bd20789923ad3505504f9e
Seita Yamashita <seita@g.ucla.edu>
2021-10-19 12:10:42 -0700

    first commit

---
+++ neuroviewer/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <myheader></myheader>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import myheader from './components/myheader'
+
+export default {
+  name: 'App',
+  components: {
+    myheader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

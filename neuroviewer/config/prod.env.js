35915847366f79e5daa1fada7535560b355d6d96
Christian Estrada <chestrada@g.ucla.edu>
2023-03-01 17:29:51 -0800

    Typo fix

--- neuroviewer/config/prod.env.js
+++ neuroviewer/config/prod.env.js
@@ -4,5 +4,5 @@ const { merge } = require('webpack-merge')
 module.exports = merge( {
   NODE_ENV: '"production"',
   API_KEY: '"<DRIVE_API_KEY>"',
-  CLIENT_ID: '"<DRIVE_CLIENT-ID>"'
+  CLIENT_ID: '"<DRIVE_CLIENT_ID>"'
 })

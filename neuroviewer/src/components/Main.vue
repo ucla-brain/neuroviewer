ea19d8c1480bc1c2dd0ed556f40c0882db96d309
Christian Estrada <chestrada@g.ucla.edu>
2023-04-19 09:31:04 -0700

    Addition of a viewer controls box

--- neuroviewer/src/components/Main.vue
+++ neuroviewer/src/components/Main.vue
@@ -19,6 +19,45 @@
       </Card>
     </form>
     <!-- <label>URL:</label><input v-model="fileurl" @keyup="readUrlFile" placeholder="fileurl" size="95" /> -->
+    <!-- <template> -->
+      <Popper>
+        <v-icon icon="mdi-help-circle" class="ctrls-icon"></v-icon>
+        <template #content>
+          <div id="content" class="ctrls-popup bg-dark"> 
+            <label class="ctrls-content"><b>Viewer Controls</b></label>
+            <div class="ctrls-text">
+              <div class="ctrls-actions">
+                <h6>Viewer Action</h6>
+                <p class="ctrl-one">Pan:</p>
+                <p class="ctrl-two">Rotate:</p>
+                <p class="ctrl-three">Zoom:</p>
+              </div>
+              <div class="ctrl-keys">
+                <h6>Controls</h6>
+                <p ><div class="arrow-keys">
+                  <v-icon class="up-arrow-key" icon="mdi-arrow-up-bold-box-outline"></v-icon>
+                  <v-icon icon="mdi-arrow-left-bold-box-outline"></v-icon>
+                  <v-icon icon="mdi-arrow-down-bold-box-outline"></v-icon>
+                  <v-icon icon="mdi-arrow-right-bold-box-outline"></v-icon>
+                </div>[<i>Arrow Keys</i>] <br>or<br> 
+                <v-icon icon="mdi-cursor-default-gesture" class="drag-key" size="30px"></v-icon><br>
+                [<i>(hold) Option + Click + Drag</i>]</p>
+                <p ><v-icon icon="mdi-cursor-default-gesture" class="drag-key" size="30px"></v-icon><br>
+                  [<i>Click + Drag</i>]</p>
+                <p >
+                <v-icon icon="mdi-mouse"></v-icon>
+                <v-icon icon="mdi-arrow-up-down"></v-icon><br>
+                [scroll wheel] <br> or <br>
+                <v-icon icon="mdi-gesture-pinch" size="30px"></v-icon><br>
+                [Touch-Pad pinch]</p>
+              </div>
+            </div>
+            <p class="ctrls-footer">[option+click] and [right-click] are interchangeable for touch-pad and mouse respectively</p>
+          </div>
+        </template>
+      </Popper>
+    <!-- </template> -->
+
     <div id="container" style='position:relative; width:100%; height:700px'> 
     </div>
     <filelist 
@@ -246,5 +285,71 @@ label {
 }
 form {
   padding-bottom: 20px;
+}
+.ctrls-icon{
+  position: absolute;
+  margin-top: -20px;
+  right: 5vw; 
+}
+.ctrls-popup{
+  background-color: rgba(75, 77, 81, 0.386);
+  color: white;
+  position: absolute;
+  right: -94.6vw;
+  width: 25vw;
+  max-width: 350px;
+  border-radius: 5px;
+  padding: 10px;
+  text-align: center;
+}
+.ctrls-content{
+  font-size: large;  
+  padding-bottom: 10px;
+}
+h6{
+  text-decoration: underline;
+}
+.ctrls-text{
+  display: grid;
+  grid-template-columns:  repeat(2, 1fr);
+  background-color: rgba(49,52,56,255);
+  text-align: center;
+  padding-top: 10px;
+  border-radius: 5px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+.ctrls-actions, .ctrl-keys{
+  display: grid;
+  grid-template-rows: 0.5fr 2fr 1fr 1.6fr;
+  gap: 5px;
+}
+.ctrl-keys > p{
+  border-left: 1px solid rgba(91, 91, 91, 0.7);
+}
+.arrow-keys{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  width: fit-content;
+  height: fit-content;
+  margin-left: auto;
+  margin-right: auto;
+}
+.up-arrow-key{
+  grid-column-start: 2;
+  grid-column-end: 4;
+  gap: 0px;
+}
+.drag-key{
+  font-size: larger;
+  height: 50px;
+  width: 50px;
+}
+.ctrls-footer{
+  padding-top: 10px;
+}
+.ctrl-one, .ctrl-two, .ctrl-three{
+  margin: auto;
 }
 </style>

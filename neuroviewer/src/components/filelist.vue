db13c5bbe4508f61e61363df6f51df94f8030605
Christian Estrada <chestrada@g.ucla.edu>
2023-04-21 14:17:48 -0700

    toggle-all button responsiveness update

--- neuroviewer/src/components/filelist.vue
+++ neuroviewer/src/components/filelist.vue
@@ -22,7 +22,7 @@
               <v-icon icon="mdi-file-check"></v-icon>
               [<span class="successful-files-count">{{ filteredValidList.length }}</span>]
               <div class="form-check form-switch">
-                    <input class="form-check-input toggleAll" type="checkbox" role="switch" id="flexSwitchCheckChecked" @click="handleToggle(null, null, $event)" checked>
+                    <input class="form-check-input toggleAll" type="checkbox" role="switch" id="allFlexSwitchCheckChecked" @click="handleToggle(null, null, $event)" checked>
               </div>
             </v-tab>
             <v-tab :value="2" :key="2" class="v-tab">
@@ -79,7 +79,8 @@ export default {
       tabIndex: 1,
       filterVal: '',
       filteredFiles: [...this.filenames, ...this.erroredFileNames],
-      allToggledFiles: []
+      allToggledFiles: [],
+      initialToggle: true,
     }
   },
   computed: {
@@ -165,7 +166,7 @@ export default {
 
     toggleAll(toggleValue){
         for (let fileIndex in this.allToggledFiles){
-          //set their toggled status to the same as the 'toggle-all' button
+          //set thetoggled status of each valid file to the same as the 'toggle-all' button
           this.allToggledFiles[fileIndex].toggled = (toggleValue ? true: false);
           if (toggleValue){
             this.loadToggledSwcFiles(this.allToggledFiles[fileIndex].name, this.allToggledFiles[fileIndex].swcIndex)
@@ -175,7 +176,6 @@ export default {
         }
 
         let toggleElement = document.getElementsByClassName('fileToggle')
-
         for (let el of toggleElement){ 
           if (el.checked != toggleValue){
             el.checked = toggleValue
@@ -218,24 +218,40 @@ export default {
     },
     
     handleToggle(event, fileName, toggleAllEvent){
-      this.createToggleList()
+      //initialize required data
+      let allItemsToggledOff = true;
+      let toggleAllBtn = document.getElementById('allFlexSwitchCheckChecked')
+      if (this.initialToggle){
+        this.createToggleList()
+        this.initialToggle = false;
+      }
+      
 
-      if (toggleAllEvent){ //for toggling all neurons
+      if (toggleAllEvent){ //for toggling all neurons at once
         const toggleVal = toggleAllEvent.target.checked
         this.toggleAll(toggleVal)
-      }else {
+      }else {               //single-neuron toggling
         let toggleStatus = event.target.checked;
         let fileNameIndex = this.filenames.indexOf(fileName);
         this.allToggledFiles[fileNameIndex].toggled = (toggleStatus ? true : false);
         this.clearViewer()
-  
+
         //reload viewer with only toggled items 
         for (let fileIndex in this.allToggledFiles){
           if ((this.allToggledFiles[fileIndex].toggled) && (fileIndex != fileNameIndex)) {
             this.loadToggledSwcFiles(this.allToggledFiles[fileIndex].name, this.allToggledFiles[fileIndex].swcIndex)
           }
+          if (this.allToggledFiles[fileIndex].toggled){
+            allItemsToggledOff = false;
+          } 
         }
-        //loads the newly toggled file last due the FIFO loading of the viewer
+        if (allItemsToggledOff) {
+            toggleAllBtn.checked = false
+        } else {
+          toggleAllBtn.checked = true
+        }
+
+        //loads the newly toggled-on file at the end, due the FIFO loading style of the viewer
         if (toggleStatus) { 
           this.loadToggledSwcFiles(this.allToggledFiles[fileNameIndex].name, this.allToggledFiles[fileNameIndex].swcIndex)
         }

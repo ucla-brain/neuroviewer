3db19e193ef6be4a1a8db795e6cfea0e797be802
Christian Estrada <chestrada@g.ucla.edu>
2025-03-24 10:29:20 -0700

    Add UCLA Brain logo to navbar

--- neuroviewer/src/components/myheader.vue
+++ neuroviewer/src/components/myheader.vue
@@ -3,21 +3,25 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <router-link class="text-white px-3" to="/">home</router-link>
-                </li>
-<!--
-                <li class="nav-item">
-                    <router-link class="text-white px-3" to="/test1">test1</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="text-white px-3" to="/test2">test2</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="text-white px-3" to="/test3">test3</router-link>
-                </li>
--->
+                    <li class="nav-item">
+                        <router-link class="text-white px-3" to="/">home</router-link>
+                    </li>
+                    <!-- <b-img src="https://uclabrain.org/wp-content/uploads/2024/12/logo-9-a2small.png" rounded width="50px" height="50px"> </b-img> -->
+                    <!--
+                        <li class="nav-item">
+                            <router-link class="text-white px-3" to="/test1">test1</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="text-white px-3" to="/test2">test2</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="text-white px-3" to="/test3">test3</router-link>
+                        </li>
+                        -->
                 </ul>
+                <a href="https://uclabrain.org/">
+                    <b-img src="https://uclabrain.org/wp-content/uploads/2024/12/logo-9-a2small.png" thumbnail width="64px" height="64px" class="mr-3"> </b-img>
+                </a>
             </div>
         </nav>
     </div>
